78186d7fbf12f285cf1fbab19999173fc1c601bb
V-i-t-h-u-n <70767367+V-i-t-h-u-n@users.noreply.github.com>
2023-09-15 15:18:50 +0530

    Quiz App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import QuizApp from './components/QuizApp/QuizApp';
+import 'bootstrap/dist/css/bootstrap.css';
+import Result from './components/Result/Result';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<QuizApp />}></Route>
+          <Route path="/result-page" element={<Result />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
