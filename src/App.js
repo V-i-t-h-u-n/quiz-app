f1baa83d2eb97dc19c27f1aa141f873088b1d8be
V-i-t-h-u-n <70767367+V-i-t-h-u-n@users.noreply.github.com>
2023-09-15 22:28:42 +0530

    removed Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import './App.css';
 import QuizApp from './components/QuizApp/QuizApp';
 import 'bootstrap/dist/css/bootstrap.css';
-import Result from './components/Result/Result';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<QuizApp />}></Route>
-          <Route path="/result-page" element={<Result />}></Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <QuizApp />
+    </div>
   );
 }
 
